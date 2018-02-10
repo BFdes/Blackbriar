83158fc294a76e6062341d55d9a500edd5088dd6
antonioe89 <eadrch@live.com>
2018-02-10 14:00:33 +0000

    added new optimization functions

--- optimization_functions.py
+++ optimization_functions.py
@@ -2,15 +2,24 @@ import numpy as np
 import random
 
 '''
-Functions that will be optimized, from website at 
-Surjanovic, S. & Bingham, D. (2013). Virtual Library of Simulation Experiments: 
-Test Functions and Datasets. 
-Retrieved January 17, 2018, from http://www.sfu.ca/~ssurjano. 
+Functions that will be optimized, from website at
+Surjanovic, S. & Bingham, D. (2013). Virtual Library of Simulation Experiments:
+Test Functions and Datasets.
+Retrieved January 17, 2018, from http://www.sfu.ca/~ssurjano.
 I'm vectorizing them right now
 '''
 
-domains = dict
-minimums = dict
+domains = dict{"sphere_sum":(-5.12,5.12),"difpow_sum":(-1,1),
+"hypEll": (-65.536,65.536),"ackley":(-32.768,-32.768),"griewank":(-600,600),
+"schwefel":(-500,500),"zakharov":(-5,10), "dixon_price":(-10,10),
+"rosenbrock":(-5,10),"rastrigin":(-5.12,5.12),"styTang"=(-5,5),
+"levy":(-10,10), "trid": (-DIM**2,DIM**2), "stepint":(-5.12,5.12),"stepS":(-5.12,5.12)}
+#Fixed for the moment, but some solutions will require the number of dimensions
+#Might also be interesting to add the global minimum coordinates, would
+#require the same
+minimums = dict{"sphere_sum":0,"difpow_sum":0,"hypEll": 0,"ackley":0,"griewank":0,
+"schwefel":0,"zakharov":0, "dixon_price":0,"rosenbrock":0,"rastrigin":0,
+"styTang":=-39.16599*DIM, "levy":0, "trid": -200, "stepint",:0,"stepS":0}
 
 #sphere function. Minimum at 0
 #domain is [-5.12,5.12] at f=0
@@ -60,12 +69,13 @@ def zakharov(dimension, variables):
     b=np.sum(0.5*np.multiply(range(1,dimension+1),variables))**2
     c=b**2
     return a+b+c
-'''
+
 ##Three hump camel function. Actually works in only 2 dimensions, 3 local minima
 #domain is [-5,5]. Minimum at f=0 for 0,0
+#TODO, VECTORIZE
 def three_hump_camel(dimension, variables):
     return (2.0*variables[0]**2) -(1.05*variables[1]**4) +((1.0/6.0)*variables[0]**6)+variables[0]*variables[1]+variables[1]**2
-'''
+
 #Dixon-Price function.
 #domain is [-10,10]. Minimum at f=0 for complex expression, with x[i]=2**((-2**i-2)/(2**i))
 def dixon_price(dimension, variables):
@@ -80,12 +90,13 @@ def rosenbrock(dimension,variables):
 #domain is [-5.12,5.12], minimum is f=0 for all 0
 def rastrigin(dimension, variables):
     return 10*dimension+np.sum(np.power(variables,2)-10*np.cos(2*np.pi*variables))
+#TODO, vectorize this one too
 '''
 #powell function
 #domain is [-4, 5], minimum at f = 0 for all 0
 def powell(dimension, variables):
     return sum( for i in xrange(1,dimension/4))
-'''    
+'''
 #stiblynsky tang function
 #[-5, 5] with minimum f=-39.16599*dimension at x= -2.903534
 def styTang(dimension, variables):
@@ -107,7 +118,7 @@ def genPenal(dimension,variables):
             u[j-1]=100*(-variables[j]-10)**4
     d=sum(u[i] for i in xrange(0,dimension))
     return (math.pi/float(dimension))*(a+b+c)+d
-#Levy function. 
+#Levy function.
 #domain is [-10,10], Minimum is at all x=1 for f=0
 def levy(dimension, variables):
     varW=1+(variables-1)/4
@@ -126,20 +137,20 @@ def trid(dimension, variables):
     a=np.sum(np.power(variables-1,2))
     b=np.sum(np.multiply(variables[1:],variables[:dimension-1]))
     return a+b
-    
-#simple integer step, 
+
+#simple integer step,
 #domain is [-5.12,5.12], Minimum is at all x=0 for f=0
 def stepint(dimension, variables):
-    return 25.0+sum(math.floor(variables[i]) for i in xrange(1,dimension+1))
+    return 25.0+np.sum(np.floor(variables))
 #a discretization of the sphere function
 #domain is [-5.12,5.12] at f=0
 def stepS(dimension, variables):
-    return sum((math.floor(variables[i]+0.5))**2 for i in xrange(1,dimension+1))
+    return np.sum(np.power(np.floor(variables+0.5),2))
 #quartic function
 #domain is very small [-1.28,1.28] with minima at all 0 for f=<15, yes there is no precise minimum
 def quartic(dimension, variables):
-    a=sum(i*(variables[i]**4) for i in xrange(1,dimension+1))
-    b=np.random.uniform(0,1)
+    a=np.sum(np.multiply(range(1,dimension+1),np.power(variables,4)))
+    b=0.5#np.random.uniform(0,1)
     return a+b
 #the beale function is only in 2 dimensions
 #the domain is between [-4.5,4.5] with the minimum at f=0 with x = 3 and 0.5
@@ -148,13 +159,13 @@ def beale(dimension, variables):
     b=(2.25-variables[1]+variables[1]*(variables[2]**2))**2
     c=(2.625-variables[1]+variables[1]*(variables[2]**3))**2
     return a+b+c
-#the easom function
+#the easom function, only 2 dimension
 #domain is [-100,100] with f=-1 at minimum with exes equal to pi
 def easom(dimension, variables):
     a=-math.cos(variables[1])*math.cos(variables[2])
     b=math.exp(-(variables[1]-math.pi)**2 - (variables[2]-math.pi)**2)
     return a*b
-#the matyas function
+#the matyas function, also 2 dimensions
 #domain is [-10,10] with f=0 at x=0
 def matyas(dimension, variables):
     return 0.26*(variables[1]**2 + variables[2]**2)-0.48*variables[1]*variables[2]
