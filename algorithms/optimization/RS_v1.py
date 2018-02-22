3e0cd4eef25a2c4d5afdbdedd57645761160294b
antonioe89 <eadrch@live.com>
2018-02-22 16:51:27 +0000

    functionalized the RS algorithm

--- algorithms/optimization/RS_v1.py
+++ algorithms/optimization/RS_v1.py
@@ -6,6 +6,8 @@ Created on Tue Feb 20 15:29:30 2018
 """
 import numpy as np
 import random as rnd
+from optimization_utilities import RS_f_v1 as RS_f
+from optimization_utilities import RS_p_v1 as RS_p
 #This version of the function is not vectorized. Explicitly does NOT use numpy facilites
 def RS_v1(f, p_best, max_iter, bounds, params=False):
     '''
@@ -25,37 +27,14 @@ def RS_v1(f, p_best, max_iter, bounds, params=False):
     1. p_best is used in case a good value is already known.
 
     '''
-    #initialization
-    best_position = p_best
-    best_value = f(p_best)
-    dim = len(p_best)
     # plotting utilities
     make_plot = params
+    dim = len(p_best)
     if dim < 1 and make_plot:
         print("warning, only 1 dimension, cannot plot")
     # creating arrays for the plots
+
     if make_plot:
-        all_trials_x = np.zeros(max_iter)
-        all_trials_y = np.zeros(max_iter)
-        all_results = np.zeros(max_iter)
-    #search loop
-    for i_iter in range(max_iter):
-        print('best_value = ',best_value)
-        # tries random values in the vecinity of the best position so far
-        p_trial = np.array([rnd.uniform(bounds[i][0],bounds[i][1]) for i in range(dim)])
-        trial_value = f(p_trial)
-        # compiling results from plot
-        if make_plot:
-            all_trials_x[i_iter] = p_trial[0]
-            all_trials_y[i_iter] = p_trial[1]
-            all_results[i_iter] = trial_value
-        #if trial values is better than best position, this gets substituted
-        #Do with changing in place
-        if trial_value < best_value:
-            best_position = np.copy(p_trial)
-            best_value  = trial_value
-    # plotting
-    if make_plot:
-        return best_value, best_position, all_results
+        return RS_p(f, p_best, max_iter, bounds)
     else:
-        return best_value, best_position
+        return RS_f(f, p_best, max_iter, bounds)
