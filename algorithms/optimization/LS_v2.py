46d3d8cb49992902eb765b7c368e80a22bedff52
antonioe89 <eadrch@live.com>
2018-02-22 16:50:55 +0000

    functionalized the LS algorithm

--- algorithms/optimization/LS_v2.py
+++ algorithms/optimization/LS_v2.py
@@ -35,35 +35,53 @@ def LS_v2(f, p_init, max_iter, bounds, radius, reduce_iter, reduce_frac, params=
     make_plot = params
     if dim < 1 and make_plot:
         print("warning, only 1 dimension, cannot plot")
-    # creating arrays for the plots
+    
+    # if we are plotting then we use
     if make_plot:
+        # creating arrays for the plots
         all_trials_x = np.zeros(max_iter)
         all_trials_y = np.zeros(max_iter)
         all_results = np.zeros(max_iter)
-    #search loop
-    for i_iter in range(max_iter):
-        print('best_value = ',best_value,'   radius = ',radius)
-        # tries random values in the vecinity of the best position so far
-        p_trial = best_position + radius * np.array([rnd.uniform(bounds[i][0],bounds[i][1]) for i in range(dim)])
-        trial_value = f(p_trial)
-        # compiling results from plot
-        if make_plot:
+        for i_iter in range(max_iter):
+            print('best_value = ',best_value,'   radius = ',radius)
+            # tries random values in the vecinity of the best position so far
+            p_trial = best_position + radius * np.array([rnd.uniform(bounds[i][0],bounds[i][1]) for i in range(dim)])
+            trial_value = f(p_trial)
+            # compiling results from plot
             all_trials_x[i_iter] = p_trial[0]
             all_trials_y[i_iter] = p_trial[1]
             all_results[i_iter] = trial_value
-        #if trial values is better than best position, this gets substituted
-        #Do with changing in place
-        if trial_value < best_value:
-            best_position = np.copy(p_trial)
-            best_value  = trial_value
-        else:
-            #check whether it's time to set radius to smaller one. Resets failcount
-            fail_count += 1
-        if fail_count == reduce_iter:
-            radius *= reduce_frac
-            fail_count = 0
-    # plotting
-    if make_plot:
+            #if trial values is better than best position, this gets substituted
+            #Do with changing in place
+            if trial_value < best_value:
+                best_position = np.copy(p_trial)
+                best_value  = trial_value
+            else:
+                #check whether it's time to set radius to smaller one. Resets failcount
+                fail_count += 1
+            if fail_count == reduce_iter:
+                radius *= reduce_frac
+                fail_count = 0
+        # plotting
         return best_value, best_position, all_results
-    else:
+    
+    # if we are not collecting results for plotting
+    else:    
+        #search loop
+        for i_iter in range(max_iter):
+            print('best_value = ',best_value,'   radius = ',radius)
+            # tries random values in the vecinity of the best position so far
+            p_trial = best_position + radius * np.array([rnd.uniform(bounds[i][0],bounds[i][1]) for i in range(dim)])
+            trial_value = f(p_trial)
+            #if trial values is better than best position, this gets substituted
+            #Do with changing in place
+            if trial_value < best_value:
+                best_position = np.copy(p_trial)
+                best_value  = trial_value
+            else:
+                #check whether it's time to set radius to smaller one. Resets failcount
+                fail_count += 1
+            if fail_count == reduce_iter:
+                radius *= reduce_frac
+                fail_count = 0
         return best_value, best_position
